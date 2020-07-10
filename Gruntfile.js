a23a81ae937ad1075936231fdaae0b6b84f06eeb
Hemanth P <blrhpm@gmail.com>
2020-07-10 18:11:36 +0530

    Website Ready

---
+++ Gruntfile.js
@@ -0,0 +1,159 @@
+'use strict';
+
+module.exports =function(grunt){
+    
+    require('time-grunt')(grunt);
+
+    require('jit-grunt')(grunt,{
+        useminPrepare: 'grunt-usemin'
+    });
+    
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css':'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        '*js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        },
+        copy: {
+            html:{
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist' 
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamin: {
+                files: [{
+                    expand: true,
+                    dor: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif,jpeg}'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context,block){
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                seporator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                lenght: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/index.html'],
+            options: {
+                assetsDirs: ['dist','dist/css','dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true 
+                },
+                files: {
+                    'dist/index.html':'dist/index.html'
+                }
+            }
+        }
+    });
+    grunt.registerTask('css',['sass']);
+    grunt.registerTask('default',['browserSync','watch']);
+    grunt.registerTask('build',[
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
+};
---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+  $(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+       // e.preventDefault();
+       // $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active-li');
+        })
+        $(this).addClass('active-li');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#Navbar a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#Navbar ul li a').removeClass("active-li");
+            currLink.addClass("active-li");
+        }
+        else{
+            currLink.removeClass("active-li");
+        }
+    });
+}
+
+
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
